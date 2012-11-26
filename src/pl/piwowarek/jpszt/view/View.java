7ba6c971d95d59ae6317aa4a142c277b0c6583de
pivovarit <gpiwowarek@gmail.com>
2012-11-26 10:24:07 +0100

    Simple frame created.

--- src/pl/piwowarek/jpszt/view/View.java
+++ src/pl/piwowarek/jpszt/view/View.java
@@ -2,14 +2,24 @@ package pl.piwowarek.jpszt.view;
 
 import java.util.concurrent.BlockingQueue;
 
+
 import javax.swing.JFrame;
 
 import pl.piwowarek.jpszt.controller.events.PsztEvent;
 
 public class View extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public View(BlockingQueue<PsztEvent> blockingQueue) {
 		// TODO Auto-generated constructor stub
+		
+		this.setSize(640, 480);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
 
