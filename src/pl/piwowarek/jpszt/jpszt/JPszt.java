0d5e0c7efd80318770a47d25747298571f58143d
pivovarit <gpiwowarek@gmail.com>
2012-11-26 10:19:13 +0100

    JPszt.java added

---
+++ src/pl/piwowarek/jpszt/jpszt/JPszt.java
@@ -0,0 +1,36 @@
+package pl.piwowarek.jpszt.jpszt;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+import pl.piwowarek.jpszt.controller.Controller;
+import pl.piwowarek.jpszt.controller.events.PsztEvent;
+import pl.piwowarek.jpszt.model.Model;
+import pl.piwowarek.jpszt.view.View;
+
+/**
+ * @author Adam Papros, Grzegorz Piwowarek, Pawe³ Piekaruœ
+ * 
+ */
+public class JPszt {
+
+	public static void main(String[] args) {
+		Controller controller;
+
+		try {
+			final Model model = new Model();
+			final BlockingQueue<PsztEvent> blockingQueue = new LinkedBlockingQueue<PsztEvent>();
+			final View view = new View(blockingQueue);
+			controller = new Controller(view, model, blockingQueue);
+			controller.run();
+		
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+	}
+
+}
